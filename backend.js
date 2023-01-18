90fb4bd3f1b05bfd9dcd172d09fbae5067e99859
alexhatch <alexhatchh@gmail.com>
2023-01-18 13:08:21 -0800

    added get for user and job name

--- backend.js
+++ backend.js
@@ -39,9 +39,15 @@ app.get('/', (req, res) => {
     res.send('Hello World!');
 });
 
+// find users by name and job 
 app.get('/users', (req, res) => {
     const name = req.query.name;
-    if (name != undefined){
+    const job = req.query.job;
+    if (name != undefined && job != undefined){
+        let result = findUserByNameAndJob(name, job);
+        result = {users_list: result};
+        res.send(result);
+    }else if (name != undefined){
         let result = findUserByName(name);
         result = {users_list: result};
         res.send(result);
@@ -82,6 +88,12 @@ function findUserById(id) {
 const findUserByName = (name) => { 
     return users['users_list'].filter( (user) => user['name'] === name); 
 }
+
+
+const findUserByNameAndJob = (name, job) => { 
+    return users['users_list'].filter( (user) => user['name'] === name && user['job'] === job); 
+}
+
 
 app.post('/users', (req, res) => {
     const userToAdd = req.body;
